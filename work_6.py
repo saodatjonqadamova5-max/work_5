e78b99c0e29d56a73ab10d85d221af344169e1de
saodatjonqadamova5-max <saodatjonqadamova5@gmail.com>
2026-03-03 17:41:26 +0500

    work

---
+++ work_6.py
@@ -0,0 +1,56 @@
+#33
+# x = int(input("x="))
+# a = int(input("a="))
+# y = int(input("y="))
+# z = (a * y) / x
+# print(z)
+#34
+# x = int(input("x="))
+# a = int(input("a="))
+# y = int(input("y="))
+# b = int(input("b="))
+# z= a / x - b / y
+# print(z)
+#35
+# v=int(input("v="))
+# u=int(input("u="))
+# t1=int(input("t1="))
+# t2=int(input("t2="))
+# s1=t1 * (v + u)
+# s2=t2 * (v - u)
+# print(s1,s2)
+#36, #37
+# s0=int(input("s0="))
+# v1=int(input("v1="))
+# v2=int(input("v2="))
+# t=int(input("t="))
+# s1=v1 * t
+# s2=v2 * t
+# s=s0+s1+s2
+# s3=s0-(s1+s2)
+# print(s, s3)
+#38
+# b=int(input("b="))
+# a=int(input("a="))
+# x= (-1) * b / a
+# print(x)
+#39
+# import math
+# A=int(input("A="))
+# B=int(input("B="))
+# C=int(input("C="))
+# D=B**2-4*A*C
+# x1=((-1)*B - math.sqrt(D))/2*A
+# x2=((-1)*B + math.sqrt(D))/2*A
+# print(x1,x2)
+#40
+# A1=int(input("A1="))
+# B1=int(input("B1="))
+# C1=int(input("C1="))
+# A2=int(input("A2="))
+# B2=int(input("B2="))
+# C2=int(input("C2="))
+# D=(A1*B2-A2*B1)
+# x=(C1*B2-C2*B1)/D
+# y=(A1*C2-A2*C1)/D
+# print(x,y)
